7f2ed1dc3579ce02438cb786d1712a1925bf7ea7
David Njuguna <dnjuguna@gmail.com>
2014-08-26 15:33:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+module.exports = {
+	"avatar" : require(__dirname + "/avatar.js"),
+	"create" : require(__dirname + "/create.js"),
+	"create-twitter" : require(__dirname + "/create-twitter.js"),
+	"redirect" : require(__dirname + "/redirect.js"),
+	"redirect-twitter" : require(__dirname + "/redirect-twitter.js")
+};
